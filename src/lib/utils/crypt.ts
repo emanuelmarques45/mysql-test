a6af911e2bd4dfb9db920b3be1c3ade0062ee73a
Emanuel Marques <emanuelfelipemarques45@gmail.com>
2023-05-28 22:46:49 -0300

    test

--- src/lib/utils/crypt.ts
+++ src/lib/utils/crypt.ts
@@ -1,21 +1,21 @@
 import { AES, enc } from "crypto-js"
 
+const SECRET_KEY = "process.env.SECRET_KEY"
+
 function encryptString(string: string) {
-  return AES.encrypt(string, process.env.SECRET_KEY).toString()
+  return AES.encrypt(string, SECRET_KEY).toString()
 }
 
 function decryptString(string: string) {
-  return AES.decrypt(string, process.env.SECRET_KEY).toString(enc.Utf8)
+  return AES.decrypt(string, SECRET_KEY).toString(enc.Utf8)
 }
 
 function encryptObject(object: object) {
-  return AES.encrypt(JSON.stringify(object), process.env.SECRET_KEY).toString()
+  return AES.encrypt(JSON.stringify(object), SECRET_KEY).toString()
 }
 
 function decryptObject(object: string) {
-  return JSON.parse(
-    AES.decrypt(object, process.env.SECRET_KEY).toString(enc.Utf8)
-  )
+  return JSON.parse(AES.decrypt(object, SECRET_KEY).toString(enc.Utf8))
 }
 
 const secret = {
