81f5a0aa09edfd66ad3ed02f9ad0db8deb43a6a7
Emanuel Marques <emanuelfelipemarques45@gmail.com>
2023-05-30 21:34:54 -0300

    ...

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -36,10 +36,10 @@ export default async function handler(
         return res.status(200).json({ token, user })
       }
 
-      res.status(401).json({ message: "Incorrect password" })
+      res.status(200).json({ message: "Incorrect password" })
     }
 
-    res.status(404).json({ message: "User not found" })
+    res.status(200).json({ message: "User not found" })
   }
 
   return res.status(405).json({ message: "Method not allowed" })
